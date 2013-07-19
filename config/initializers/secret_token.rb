cdfe1e90b9046fa5ad916affd7e0764215c745ae
Onur Ozgur OZKAN <onur.ozgur.ozkan@lab2023.com>
2013-07-19 10:18:09 +0300

    #5 setup rails project with cybele

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TisikkirlirSipirmin::Application.config.secret_key_base = 'b89a1dc0c343c951fe944767a0b511b390b9e38e867241e515dc1e6e7a46a6135ff4926ff82f00cdd4bde943ccc2783aed25292043e10e081e8ca4699fdc7a36'
